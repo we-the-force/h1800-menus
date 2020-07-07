51692c2a20fec8f58d56cbde7ed90cf96afb11fe
Will Alvarez <will.alvarez@gmail.com>
2020-07-07 17:39:36 -0500

    cambios finales

--- src/js/routes.js
+++ src/js/routes.js
@@ -3,8 +3,28 @@ import HomePage from '../pages/home.f7.html';
 
 var routes = [
   {
-    path: '/',
-    component: HomePage,
+    path: '(.*)',
+    // pushStateRoot: 'menus',
+    async: function(routeTo, reouteFrom, resolve, reject){
+      var router = this;
+
+      var app = router.app;
+      app.preloader.show('Cargando...','white');
+      app.request.promise.json('https://hacienda1800.com/panel/api/collections/get/menu').then(function(res){
+        console.log(res);
+        app.preloader.hide();
+        resolve(
+          {
+            component: HomePage
+          },
+          {
+            context: {
+              menu: res.data.entries
+            }
+          }
+        );
+      });
+    },
   },
 ];
 
